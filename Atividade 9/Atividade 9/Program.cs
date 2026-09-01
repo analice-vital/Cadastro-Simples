fd5878a6ff7dc7a14a698e5b0d944cb51a6c7fbb
Analice Vital Pereira <analicevitalpereira@gmail.com>
2026-09-01 15:02:15 -0300

    Add files via upload

---
+++ Atividade 9/Atividade 9/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // EXERCÍCIO 09 - PERFIL DE USUÁRIO
+
+        Console.WriteLine("=================================");
+        Console.WriteLine("        PERFIL DO USUÁRIO");
+        Console.WriteLine("=================================");
+
+        Console.Write("Nome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Nome de usuário: ");
+        string usuario = Console.ReadLine();
+
+        Console.Write("Idade: ");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Cidade: ");
+        string cidade = Console.ReadLine();
+
+        Console.Write("Altura: ");
+        double altura = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("O perfil está ativo? (true/false): ");
+        bool ativo = Convert.ToBoolean(Console.ReadLine());
+
+        Console.WriteLine();
+        Console.WriteLine("=================================");
+        Console.WriteLine("        PERFIL DO USUÁRIO");
+        Console.WriteLine("=================================");
+
+        Console.WriteLine("Nome: " + nome);
+        Console.WriteLine("Usuário: " + usuario);
+        Console.WriteLine("Idade: " + idade + " anos");
+        Console.WriteLine("Cidade: " + cidade);
+        Console.WriteLine("Altura: " + altura + " metros");
+        Console.WriteLine("Perfil ativo: " + ativo);
+
+        Console.ReadKey();
+    }
+}
